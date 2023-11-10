ae1493b84db17dc8d5aac90fee343943b10d6ab4
Tino Lorenz <tino.lorenz@gmx.net>
2023-11-10 18:41:50 +0100

    Implement copy tool

--- src/app.rs
+++ src/app.rs
@@ -3,11 +3,29 @@ use egui::{Sense, Rounding, Color32, Stroke, Pos2, Rect};
 
 pub struct ImgProcDemo {
     src_grid: PixGrid,
+    dst_grid: PixGrid,
+    tool: Tool,
 }
 
 enum Tool {
     Pen (u8),
     Conv,
+    Cpy,
+}
+
+impl Tool {
+    fn interact(&self, src_grid: &mut PixGrid, dst_grid: &mut PixGrid) {
+        match self {
+            Tool::Cpy => {
+                if let Some((ix, iy)) = src_grid.hovered_idx {
+                    if src_grid.pressed {
+                        dst_grid.try_set(ix as i32, iy as i32, src_grid.get_clamped(ix as i32, iy as i32));
+                    }
+                }
+            },
+            _ => ()
+        }
+    }
 }
 
 struct PixGrid {
@@ -17,6 +35,9 @@ struct PixGrid {
 
     // ui stuff
     pix_size: u32,
+    margin: u32,
+    hovered_idx: Option<(u32, u32)>,
+    pressed: bool,
 }
 
 impl PixGrid {
@@ -26,6 +47,9 @@ impl PixGrid {
             height,
             pixels: Vec::with_capacity(height as usize),
             pix_size,
+            margin: 2,
+            hovered_idx: None,
+            pressed: false
         };
         for y in 0..height {
             let mut row_vec = Vec::with_capacity(width as usize);
@@ -64,16 +88,25 @@ impl PixGrid {
     }
 
     // for now just draw, no interactivity
-    fn draw(&self, ui: &mut egui::Ui) {
-        // use a fixed size
-        let margin = 2;
+    fn draw(&mut self, ui: &mut egui::Ui) {
         // to draw n pixel boxes, we want n * pixel size and (n+1) * margin size
         let desired_size = egui::vec2(
-            (self.width * (self.pix_size + margin) + margin) as f32,
-            (self.height * (self.pix_size + margin) + margin) as f32);
-        let (rect, mut _response) = ui.allocate_exact_size(desired_size, Sense::click_and_drag());
+            (self.width * (self.pix_size + self.margin) + self.margin) as f32,
+            (self.height * (self.pix_size + self.margin) + self.margin) as f32);
+        let (rect, response) = ui.allocate_exact_size(desired_size, Sense::click_and_drag());
 
-        // check response for click
+        // check for hover and mouse press (button down, not just click so tools can be dragged)
+        if let Some(pos) = response.hover_pos() {
+            let pos = pos - rect.min.to_vec2();
+            // clamping in case the pointer is on the last margin
+            let ix = (pos.x as u32 / (self.pix_size + self.margin)).clamp(0, self.width - 1);
+            let iy = (pos.y as u32 / (self.pix_size + self.margin)).clamp(0, self.height - 1);
+            self.hovered_idx = Some((ix, iy));
+            self.pressed = response.is_pointer_button_down_on();
+        } else {
+            self.hovered_idx = None;
+            self.pressed = false;
+        }
 
         if ui.is_rect_visible(rect) {
             // first, draw the background:
@@ -82,9 +115,9 @@ impl PixGrid {
 
             // draw the pixel boxes. could also cache them.
             for y_idx in 0..self.height {
-                let y = ((margin + self.pix_size) * y_idx + margin) as f32 + rect.min.y ;
+                let y = ((self.margin + self.pix_size) * y_idx + self.margin) as f32 + rect.min.y ;
                 for x_idx in 0..self.width {
-                    let x = ((margin + self.pix_size) * x_idx + margin) as f32 + rect.min.x;
+                    let x = ((self.margin + self.pix_size) * x_idx + self.margin) as f32 + rect.min.x;
                     painter.rect(
                         Rect::from_min_max(Pos2::new(x, y), Pos2::new(x + self.pix_size as f32, y + self.pix_size as f32)),
                         Rounding::ZERO,
@@ -103,6 +136,8 @@ impl ImgProcDemo {
     pub fn new(_cc: &eframe::CreationContext<'_>) -> Self {
         let mut s = Self {
             src_grid: PixGrid::new(20, 12, 200, 16),
+            dst_grid: PixGrid::new(20, 12, 100, 16),
+            tool: Tool::Cpy,
         };
         s.src_grid.try_set(5, 2, 0);
         s
@@ -114,6 +149,8 @@ impl eframe::App for ImgProcDemo {
         egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Image Processing Demo");
             self.src_grid.draw(ui);
+            self.dst_grid.draw(ui);
+            self.tool.interact(&mut self.src_grid, &mut self.dst_grid);
         });
     }
 }
